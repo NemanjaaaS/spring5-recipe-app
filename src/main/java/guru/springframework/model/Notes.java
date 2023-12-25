c11596d78aab3bd608e7c3627619382f23eee8af
NemanjaaaS <66557612+NemanjaaaS@users.noreply.github.com>
2023-12-25 15:35:52 +0100

    creating models and relationships

---
+++ src/main/java/guru/springframework/model/Notes.java
@@ -0,0 +1,38 @@
+package guru.springframework.model;
+
+import javax.persistence.*;
+import javax.sound.midi.Receiver;
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Recipe recipe;
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getNotes() {
+        return recipeNotes;
+    }
+
+    public void setNotes(String notes) {
+        this.recipeNotes = notes;
+    }
+}
